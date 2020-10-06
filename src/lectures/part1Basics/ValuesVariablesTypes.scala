b562796175ed5a77a94d0fb95f2800ffaf8276e9
anabneri <ananeri32@hotmail.com>
2020-10-05 22:58:35 -0300

    adding values

--- src/lectures/part1Basics/ValuesVariablesTypes.scala
+++ src/lectures/part1Basics/ValuesVariablesTypes.scala
@@ -2,6 +2,30 @@ package lectures.part1Basics
 
 object ValuesVariablesTypes extends App {
 
+  // val's sao imutaveis
+  // os tipos das variaveis sao opicionais
+
   val x: Int = 42
   println(x)
+
+  //vals are immutable
+  // compiler can infer types
+
+  val aString: String = "Hello test"
+
+  val aBoolean: Boolean = false
+  val aChar: Char = 'a'
+  val anInt: Int = x
+  val aShort: Short = 4613
+  val aLong: Long = 5273985273895237L
+  val aFloat: Float = 2.0f
+  val aDouble: Double = 3.14
+
+  // variables
+  var aVariable: Int = 4
+
+  aVariable = 5 // side effects
+
+
+
 }
